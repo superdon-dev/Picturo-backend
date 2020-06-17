7708549f3f1d3a2170cbd3a1ec949010b7b898c0
Superdon <logobysuperdon@gmail.com>
2020-06-17 15:28:29 +0200

    added places routes and controller

---
+++ routes/places-routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const placesController = require("../controllers/places-controller");
+
+router.get("/:pid", placesController.getPlaceById);
+router.get("/user/:uid", placesController.getPlacesByUserId);
+router.post("/", placesController.createPlace);
+router.patch("/:pid", placesController.updatePlace);
+router.delete("/:pid", placesController.deletePlace);
+
+module.exports = router;
